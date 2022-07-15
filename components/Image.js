ffa54701026eb9b9745d1339859cea4d7fcdb682
joaofaveri <joao.faveri@gmail.com>
2022-07-15 14:03:29 -0300

    Prettier config and Using the Image Component

---
+++ components/Image.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const ProfileImage = () => {
+  <Image src="images/profile.png" height={144} width={144} alt="User Name" />;
+};
+
+export default ProfileImage;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -13,17 +13,17 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Read{' '}
-          <Link href='/posts/first-post'>
+          Read{" "}
+          <Link href="/posts/first-post">
             <a>this page</a>
           </Link>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
       </main>
     </div>
-  )
+  );
 }
